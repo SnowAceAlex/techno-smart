13e0a11a8d0a1c53b571e4604ce7f20e3cd0a876
Kong Dinh <alexvinh2911@gmail.com>
2025-12-01 16:25:40 +0700

    Refactor Footer component to replace SubText with a more flexible div structure for copyright information, enhancing layout and styling consistency.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 import Container from "./Container";
 import FooterTop from "./FooterTop";
 import FooterBottom from "./FooterBottom";
-import { SubText } from "./ui/text";
 import Logo from "./Logo";
 
 const Footer = () => {
@@ -11,10 +10,11 @@ const Footer = () => {
         <FooterTop />
         <FooterBottom />
         <div className="border-t py-4">
-          <SubText className="text-center">
-            &copy; {new Date().getFullYear()} <Logo className="text-sm" />. All
-            rights reserved.
-          </SubText>
+          <div className="flex flex-wrap items-center justify-center gap-2 text-center text-gray-600 text-sm">
+            <span>&copy; {new Date().getFullYear()}</span>
+            <Logo className="text-sm" />
+            <span>All rights reserved.</span>
+          </div>
         </div>
       </Container>
     </footer>
