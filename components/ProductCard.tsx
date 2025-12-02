ed2f7fa92f874293d79a1845025d35a4926118b0
Kong Dinh <alexvinh2911@gmail.com>
2025-12-02 23:02:52 +0700

    Enhance ProductCard and ProductGrid components with improved image handling and loading states. Update next.config.ts to allow images from Sanity CDN, add new CSS variable for background color, and update package dependencies for better performance.

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,5 +1,27 @@
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+import { Product } from "@/sanity.types";
+import { urlFor } from "@/sanity/lib/image";
+import Image from "next/image";
+
+const ProductCard = ({ product }: { product: Product }) => {
+  return (
+    <div className="text-sm border border-dark_blue rounded-md bg-white group">
+      <div className="relative group overflow-hidden bg-light_bg">
+        {product?.images && (
+          <Image
+            src={urlFor(product?.images[0]).url()}
+            alt={product?.name || ""}
+            loading="lazy"
+            width={700}
+            height={700}
+          />
+        )}
+        {product?.status === "sale" && (
+          <p className="absolute top-2 left-2 z-10">Sale!</p>
+        )}
+      </div>
+      <div className="p-3">Product Details</div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -4,16 +4,18 @@ import { useEffect, useState } from "react";
 import ProductTabBar from "./ProductTabBar";
 import { productType } from "@/constants/data";
 import { client } from "@/sanity/lib/client";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "motion/react";
 import { Loader2 } from "lucide-react";
+import NoProductCard from "./NoProductCard";
 import ProductCard from "./ProductCard";
+import { Product } from "@/sanity.types";
 
 const query = `*[_type == "product" && variant == $variant] | order(name asc){
   ...,"categories": categories[]->title
 }`;
 
 const ProductGrid = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(false);
   const [tabBar, setTabBar] = useState(productType[0].title || "");
 
@@ -43,8 +45,27 @@ const ProductGrid = () => {
             <span>Product is loading...</span>
           </motion.div>
         </div>
+      ) : products.length > 0 ? (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5 mt-10">
+          {products?.map((product) => (
+            <AnimatePresence key={product?._id}>
+              <motion.div
+                layout
+                initial={{ opacity: 0.2 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <ProductCard product={product} />
+              </motion.div>
+            </AnimatePresence>
+          ))}
+        </div>
       ) : (
-        <div></div>
+        <NoProductCard
+          tabBar={tabBar}
+          className="flex flex-col items-center justify-center py-10 min-h-80 space-y-4 text-center bg-gray-100 rounded-lg w-full mt-10"
+        />
       )}
     </div>
   );
